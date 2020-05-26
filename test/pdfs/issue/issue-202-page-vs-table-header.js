1b3a2b22e25c37330aa1222fa4b50d959a15197b
David Feng <dfeng@bglcorp.com.au>
2020-05-26 15:52:07 +1000

    delete files

--- test/pdfs/issue/issue-202-page-vs-table-header.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(doc) {
-  doc.header().text("Page Header")
-
-  const table = doc.table({widths: [null]})
-  table.header().cell("Table header")
-  table.row().cell("Content")
-}
